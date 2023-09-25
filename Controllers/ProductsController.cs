ffb01744bbf4fdac80ec52a207711e5f6758753d
hady ahmed <hadyahmed57@hotmail.com>
2023-09-26 00:33:22 +0300

    Updates

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using skinet.Core.Entities;
-using skinet.Infrastructure.Data;
+using skinet.Core.Interfaces;
 
 namespace skinet.Controllers
 {
@@ -9,24 +8,36 @@ namespace skinet.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repo;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _repo.GetProductsAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductsByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProdoctBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProdocttypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using skinet.Core.Interfaces;
 using skinet.Infrastructure.Data;
 
 namespace skinet
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +16,8 @@ namespace skinet
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<StoreContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConStr1")));
+            builder.Services.AddDbContext<StoreContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConStr1"), b => b.MigrationsAssembly("skinet")));
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
             var app = builder.Build();
 
@@ -31,8 +33,26 @@ namespace skinet
             app.UseAuthorization();
 
             app.MapControllers();
+            using ( var scope = app.Services.CreateScope() )
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
+                }
+                catch ( Exception ex )
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+            }
 
             app.Run();
+
+            //Seeding Logic
         }
     }
 }
